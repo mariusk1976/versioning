49eca035a5c20444bd020ded6ba3c094b700bcab
Marius Karkosch <marius.karkosch@metrosystems.net>
2018-07-12 13:17:49 +0200

    .gitigrnoe added

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let http = require('http');
 let pjson = require('./package.json');
 console.log(pjson.version);
 
+require('pkginfo')(module, 'version');
+console.log('Version of pkginfo: ' + module.exports.version)
+
 var server = http.createServer(function(req, res) {
 res.writeHead(200);
 res.end('Hi everybody! Version from package.json:' + pjson.version);
