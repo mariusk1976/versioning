d4c8b69f8734b403928012fb191b5f9b789738ab
Marius Karkosch <marius.karkosch@metrosystems.net>
2018-07-12 12:57:47 +0200

    output of version added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var http = require('http');
+let http = require('http');
+
+let pjson = require('./package.json');
+console.log(pjson.version);
 
 var server = http.createServer(function(req, res) {
 res.writeHead(200);
-res.end('Hi everybody!');
+res.end('Hi everybody! Version from package.json:' + pjson.version);
 });
 console.log('Started')
 server.listen(8081);
